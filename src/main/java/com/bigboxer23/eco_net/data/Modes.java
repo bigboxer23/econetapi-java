26cb4b58cb0e5cb2fe16a796ae745a1ca99c79d5
Matt Jones <bigboxer23@gmail.com>
2024-10-14 11:34:18 -0500

    feat: add ability to change modes on the heater

--- src/main/java/com/bigboxer23/eco_net/data/Modes.java
+++ src/main/java/com/bigboxer23/eco_net/data/Modes.java
@@ -6,8 +6,14 @@ import lombok.Data;
 /** */
 @Data
 public class Modes {
+	public static final int HEAT_PUMP_MODE = 0;
+	public static final int VACATION_MODE = 1;
+
 	private Constraints constraints;
 
 	@Json(name = "status")
 	private String activeMode;
+
+	@Json(name = "value")
+	private int activeValue;
 }
